ac46fa758c45e2b8c07acfc67c445d2f0474240b
eeeeddddyyy <edriannegraceb@gmail.com>
2024-09-14 15:52:28 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log("Page loaded successfully!");
+    console.log("Hobbies page loaded successfully!");
+    console.log("Gallery page loaded successfully!");
+});
